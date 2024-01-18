ac6fc0bd9f6711d54b203e05cabc2b722c3c2550
fabinQ <maciejfabinski@gmail.com>
2024-01-18 20:21:48 +0100

    Dodanie nowych funkcji do app.py oraz dodanie hmtl bazowego

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return render_template("glowna.html")
 
 
 @app.route("/czesc")
-def czesc():
-    return """
-        <html>
-        <head><title>Czesc!</title></head>
-        <body>
-            <h1>Cześć wam!</h1>
-            </body>
-            </html>
-    """
-
-
-@app.route("/hej/<imie>")
-def powitanie(imie):
-    return f"Cześć, {imie}"
+@app.route("/czesc/<imie>")
+def czesc(imie=None):
+    return render_template("czesc.html", imie = imie)
 
 
 @app.route("/users/<int:id>")
