cc9e42779fe620364a07d8258e97be19fdb8a252
fabinQ <maciejfabinski@gmail.com>
2024-01-21 18:17:10 +0100

    zmiany w html i app.pt

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+# Musimy ustalić gdzie będzie nasza baza danych. Dla innych baz trzeba wpisać użytkownika i hasło
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 
-@app.route("/")
-def hello():
-    return render_template("glowna.html")
+db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
 
-@app.route("/czesc")
-@app.route("/czesc/<imie>")
-def czesc(imie=None):
-    return render_template("czesc.html", imie = imie)
-
-
-@app.route("/users/<int:id>")
-def info_users(id):
-    return f"Dane użytkownika o id {id}"
+@app.route("/p", methods=['GET','POST'])
+def todo():
+    task = []
+        if request.method == 'POST':
+            task.append(request.from['task'])
+        return render_template('todo.html', tasks=tasks)
